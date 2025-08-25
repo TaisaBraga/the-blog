978009b1a791c884adc31c5f05ee197541366e1b
Taisa Braga <taisabraga.adm@gmail.com>
2025-08-25 18:27:41 +0100

    create main container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
+import Container from "@/components/Container";
 import PostsList from "@/components/PostsList";
 import SpinLoader from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <Container>
       <header>
-        <h1 className="text-6xl font-bold text-center">HEADER</h1>
+        <h1 className="text-6xl font-bold text-center py-8">HEADER</h1>
       </header>
 
-      <Suspense fallback={<SpinLoader className='min-h-20'/>}>
+      <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
 
       <footer>
-        <h1 className="text-6xl font-bold text-center">FOOTER</h1>
+        <h1 className="text-6xl font-bold text-center py-8">FOOTER</h1>
       </footer>
-    </div>
+    </Container>
   );
 }
