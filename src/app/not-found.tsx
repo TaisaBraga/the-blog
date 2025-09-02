216e8573f97ece4b382ac39a8d8884892271bc75
Taisa Braga <taisabraga.adm@gmail.com>
2025-09-02 14:54:25 +0100

    create errormessage component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,12 @@
+import ErrorMessage from "@/components/ErrorMessage";
 import React from "react";
 
 export default function NotFoundPage() {
   return (
-    <>
-      <title>Page not found</title>
-      <div className="min-h-[320px] bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center">
-        <div>
-          <h1 className="text-7xl/tight font-extrabold mb-8">404</h1>
-          <p>404 error - The page you are trying to access does not exist.</p>
-        </div>
-      </div>
-    </>
+    <ErrorMessage
+      pageTitle="Page not found"
+      contentTitle="404"
+      content="404 error - The page you are trying to access does not exist."
+    />
   );
 }
---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+type ErrorMessageProps = {
+  pageTitle: string;
+  contentTitle: string;
+  content: React.ReactNode;
+};
+
+export default function ErrorMessage({
+  pageTitle,
+  contentTitle,
+  content,
+}: ErrorMessageProps) {
+  return (
+    <>
+      <title>{pageTitle}</title>
+      <div className="min-h-[320px] bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center">
+        <div>
+          <h1 className="text-7xl/tight font-extrabold mb-8">{contentTitle}</h1>
+          <div>{content}</div>
+        </div>
+      </div>
+    </>
+  );
+}
