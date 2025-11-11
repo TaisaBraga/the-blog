f5c94bae53097a44aa34bd96f95cafd7d435188e
Taisa Braga <taisabraga.adm@gmail.com>
2025-11-11 18:44:08 +0000

    create image upload component

--- src/actions/upload/upload-image-action.ts
+++ src/actions/upload/upload-image-action.ts
@@ -1,5 +1,8 @@
 "use server";
 
+import { mkdir, writeFile } from "fs/promises";
+import { extname, resolve } from "path";
+
 type uploadImageActionResult = {
   url: string;
   error: string;
@@ -9,6 +12,9 @@ export async function uploadImageAction(
   formData: FormData
 ): Promise<uploadImageActionResult> {
   const imageMaxSize = 921600;
+  const IMAGE_UPLOAD_DIRECTORY = "uploads";
+  const IMAGE_SERVER_URL = "http://localhost:3000/uploads";
+
   const makeResult = ({ url = "", error = "" }) => {
     return { url, error };
   };
@@ -27,9 +33,28 @@ export async function uploadImageAction(
     return makeResult({ error: "The file is too big!" });
   }
 
-  if(!file.type.startsWith('image/')){
+  if (!file.type.startsWith("image/")) {
     return makeResult({ error: "Invalid image!" });
-
   }
-  return makeResult({ url: "URL" });
+
+  const fileExtension = extname(file.name);
+  const uniqueFileName = `${Date.now()}${fileExtension}`;
+
+  //create the upload file, only once
+  const uploadFullPath = resolve(
+    process.cwd(),
+    "public",
+    IMAGE_UPLOAD_DIRECTORY
+  );
+  await mkdir(uploadFullPath, {recursive: true});
+
+  //JS <- bytes -> Node -> salvar
+  const fileArrayBuffer = await file.arrayBuffer()
+  const buffer= Buffer.from(fileArrayBuffer)
+
+  const fileFullPath = resolve(uploadFullPath, uniqueFileName)
+  await writeFile(fileFullPath, buffer)
+  const url = `${IMAGE_SERVER_URL}/${uniqueFileName}`
+
+  return makeResult({ url });
 }
